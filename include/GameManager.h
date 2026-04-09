808f6ca4e455609fcb984b8dc1544c6098c25097
Nashaat324 <youssef11926@gmail.com>
2026-04-09 11:24:11 +0200

    first version GameManager

--- include/GameManager.h
+++ include/GameManager.h
@@ -1,1 +1,25 @@
 // GameManager.h - Game logic and state management
+// GameManager.h - Game logic and state management
+
+#indef GAMEMANAGER_H
+#indef GAMEMANAGER_H
+
+#include <iostream>
+using namespace std;
+
+class GameManager{
+private:
+
+Player* player;
+Enemy* enemy;
+Gamestate state;
+void handleCombat();
+
+
+public:
+
+GameManager(Player* p, Enemy* e);
+void update();
+~GameManager();
+
+};
--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,1 +1,47 @@
-// GameManager.cpp - Game manager implementation
+#include <iostream>
+#include "GameManager.h"
+using namespace std;
+
+{
+GameManager::GameManager(Player* p,Enemy* e) : player(p), enemy(e), state(GameState::Playing)
+{
+   
+}
+  
+void GameManager::handleCombat()
+
+{
+  float dx = player->getX() - enemy->getX();
+  float dy = player->getY() - enemy->getY();
+
+  if(abs(dx) < 10 && abs(dy) < 10)
+  {
+    player->attack(enemy);
+    enemy->attack(player);
+  }
+}
+
+
+void GameManager::update()
+{
+ if (state != PLAYING)
+ {
+  return;
+ }
+ player -> move();
+ enemy->updateAI(player);
+ handleCombat();
+ if(!player->isAlive() || !enemy->isAlive())
+ {
+  state = GAME_OVER;
+ }
+
+}
+
+GameManager::~GameManager(){
+
+  cout << "GameManager destructor is called";
+
+}
+
+}// GameManager.cpp - Game manager implementation// GameManager.cpp - Game manager implementation
